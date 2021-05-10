b3e1f54637a5f76e0e31a023fd10832cfa84d809
Nicolas de Chevigné <nico2che@gmail.com>
2021-05-10 16:22:34 +0200

    Add build command

---
+++ bin/build.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const root = `${__dirname}/..`;
+const buildDir = `${root}/dist/`;
+if (!fs.existsSync(buildDir)) {
+  fs.mkdirSync(buildDir);
+}
+
+const translations = [];
+for (const filename of fs.readdirSync(`${root}/translations`)) {
+  const { definitions } = require(`${root}/translations/${filename}`, "utf-8");
+  translations.push(...definitions);
+}
+fs.writeFileSync(`${buildDir}/translations.json`, JSON.stringify(translations));
+
+const technologies = [];
+for (const filename of fs.readdirSync(`${root}/technologies`)) {
+  technologies.push(require(`${root}/technologies/${filename}`, "utf-8"));
+}
+fs.writeFileSync(`${buildDir}/technologies.json`, JSON.stringify(technologies));
