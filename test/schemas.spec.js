5d2ec30821c2b06b8486c7bd7b2bafe669ac58dd
Nicolas de Chevigné <nico2che@gmail.com>
2021-05-10 16:22:21 +0200

    Improve tests

--- test/schemas.spec.js
+++ test/schemas.spec.js
@@ -14,30 +14,17 @@ fs.readdirSync(`${root}/schemas/`)
   .map((file) => [file, require(`${root}/schemas/${file}`)])
   .map(([file, schema]) => ajv.addSchema(schema, `../schemas/${file}`));
 
-const translations = fs.readdirSync(`${root}/translations`);
-for (const filename of translations) {
-  try {
-    const definition = require(`${root}/translations/${filename}`, "utf-8");
-    const valid = ajv.validate(definition);
-    if (!valid) {
-      throw valid;
+for (const schema of ["translations", "technologies"]) {
+  for (const filename of fs.readdirSync(`${root}/${schema}`)) {
+    try {
+      const definition = require(`${root}/${schema}/${filename}`, "utf-8");
+      const valid = ajv.validate(definition);
+      if (!valid) {
+        throw valid;
+      }
+    } catch (e) {
+      console.log(e);
+      console.log(`invalid schema: ${filename}`);
     }
-  } catch (e) {
-    console.log(e);
-    console.log(`invalid schema: ${filename}`);
-  }
-}
-
-const technologies = fs.readdirSync(`${root}/technologies`);
-for (const filename of technologies) {
-  try {
-    const definition = require(`${root}/technologies/${filename}`, "utf-8");
-    const valid = ajv.validate(definition);
-    if (!valid) {
-      throw valid;
-    }
-  } catch (e) {
-    console.log(`> Invalid schema: ${filename}`);
-    console.log(e);
   }
 }
