20a432361742628b3aef4d4a26e43051ed69ba21
Nicolas de Chevigné <nico2che@gmail.com>
2021-05-11 17:21:27 +0200

    Add "slugs" definition type

--- definitions/test/schemas.spec.js
+++ definitions/test/schemas.spec.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const ajv = new Ajv({ allError: true });
 
 ajv.addKeyword("translations");
+ajv.addKeyword("slugs");
 ajv.addKeyword("condition");
 ajv.addKeyword("technology_id");
 ajv.addKeyword("hostnameReplacements");
@@ -15,7 +16,7 @@ fs.readdirSync(`${root}/schemas/`)
   .map((file) => [file, require(`${root}/schemas/${file}`)])
   .map(([file, schema]) => ajv.addSchema(schema, `../schemas/${file}`));
 
-for (const schema of ["translations", "technologies"]) {
+for (const schema of ["translations", "technologies", "slugs"]) {
   for (const filename of fs.readdirSync(`${root}/${schema}`)) {
     const definition = require(`${root}/${schema}/${filename}`, "utf-8");
     const valid = ajv.validate(definition);
